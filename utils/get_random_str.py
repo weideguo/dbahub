bd87ea741c216abe4ed8c50738624e49a266be53
wdg <2515717245@qq.com>
2018-08-21 23:47:43 +0800

    Create get_random_str.py

---
+++ utils/get_random_str.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+import random
+
+number_str="1234567890"
+special_identifier="~!@#$%^&*-=[];',./<>?:{}|_+"
+lower_alphabet_str="qwertyuiopasdfghjklzxcvbnm"
+upper_alphabet_str="QWERTYUIOPASDFGHJKLZXCVBNM"
+
+basic_str=number_str+special_identifier+lower_alphabet_str+upper_alphabet_str
+
+bs_len=len(basic_str)
+
+def get_next_unique_char(origin_str):
+    next_unique_char=basic_str[random.randint(0,bs_len-1)]
+    if origin_str[-1]==next_unique_char:
+        next_unique_char=get_next_unique_char(origin_str)
+    return next_unique_char
+
+def get_simple_random_str(n):
+    generated_str=str(basic_str[random.randint(0,bs_len-1)])	
+    for i in range(n):
+        generated_str=str(generated_str)+str(get_next_unique_char(generated_str))
+    generated_str=generated_str.rstrip(generated_str[-1])
+    return generated_str	
+
+def get_universal_random_str(n):
+    universal_generated_str=''
+    for i in range(n):
+        universal_generated_str=universal_generated_str+unichr(random.randint(0x0001,0x9FA5)) 
+    return universal_generated_str
+
+if __name__=='__main__':
+    print get_simple_random_str(8)
