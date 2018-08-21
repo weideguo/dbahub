08a5803e77a2f00b6feb86efce7384d48f33c9f9
wdg <2515717245@qq.com>
2018-08-22 00:05:44 +0800

    Create base_httpserver_demo.py

---
+++ demo/http/base_httpserver_demo.py
@@ -0,0 +1,40 @@
+#!/usr/env python
+# -*- coding: UTF-8 -*-
+
+import os   													
+import re   													
+import urllib   												
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler  	
+
+class TestHTTPHandler(BaseHTTPRequestHandler):
+    #GET  
+    def do_GET(self):
+        
+        print 'URL=',self.path
+        
+        templateStr = '''
+        <html>   
+        <head>   
+        <title>QR Link Generator</title>   
+        </head>   
+        <body>   
+        hello Python!
+        </body>   
+        </html>
+        '''
+    
+        self.protocal_version = 'HTTP/1.1'  	
+        self.send_response(200) 				 
+        self.send_header("Welcome", "Contect")  
+        self.end_headers()
+        self.wfile.write(templateStr)   		
+    def do_POST(self):
+        self.wfile.write("<p>this is get</p>")   #必须为html格式
+		
+		
+def start_server(port):
+    http_server = HTTPServer(('', int(port)), TestHTTPHandler)
+    http_server.serve_forever() 
+
+if __name__ == "__main__":
+  start_server(8000)  			
