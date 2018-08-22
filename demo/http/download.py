f5967b62f1a909739d68f731a9e3d825de806b19
wdg <2515717245@qq.com>
2018-08-22 23:18:19 +0800

    Update download.py

--- demo/http/download.py
+++ demo/http/download.py
@@ -2,7 +2,7 @@
 #--encoding=utf-8
 from urllib import urlopen
 
-f=open('C:\Users\weideguo\Desktop\weideguo\Demo2.zip','wb')            	  
+f=open('/data/Demo2.zip','wb')            	  
 u=urlopen('https://github.com/rest-client/rest-client/archive/master.zip')
 f.write(u.read())
 f.close()
