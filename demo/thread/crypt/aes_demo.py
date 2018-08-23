4141081db3363e4ed99eda363e03b233b085f7f7
wdg <2515717245@qq.com>
2018-08-23 22:54:48 +0800

    Update aes_demo.py

--- demo/thread/crypt/aes_demo.py
+++ demo/thread/crypt/aes_demo.py
@@ -5,8 +5,6 @@ import base64
 
 key="qwertyuiopasdfgh"    #加密时使用的key，只能是长度16,24和32的字符串 #大小写敏感
 
-
-
 def aes_encrypt(data,key):  
     BS = AES.block_size
     pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
@@ -15,9 +13,7 @@ def aes_encrypt(data,key):
     result = base64.b64encode(encrypted)  #base64 encode
     return result
 
-
 def aes_decrypt(en_data,key):
-
     unpad = lambda s : s[0:-ord(s[-1])]
     cipher = AES.new(key)
     result2 = base64.b64decode(data)
