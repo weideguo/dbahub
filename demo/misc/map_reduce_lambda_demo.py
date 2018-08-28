848ad5c0f70d40c00782e85d4c7d672bba428820
wdg <2515717245@qq.com>
2018-08-29 00:13:51 +0800

    Create map_reduce_lambda_demo.py

---
+++ demo/misc/map_reduce_lambda_demo.py
@@ -0,0 +1,20 @@
+#-*-encoding:utf-8-*-
+
+def f1(x):
+	return x
+
+def f2(x,y):
+	return x+y
+	
+	
+h=[10,11,12]
+map_result=map(f1,h)      
+reduce_result=reduce(f2,h)
+print('map: '+str(map_result))
+print('reduce '+str(reduce_result))
+
+
+
+f=lambda x,y,z:x*y*z
+	
+f(1,2,3)	##输出为1*2*3
