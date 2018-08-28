110377a66dd824b862906803f6429d98cb4a3fe2
wdg <2515717245@qq.com>
2018-08-29 00:29:06 +0800

    Update qr_demo.py

--- demo/misc/qr_demo.py
+++ demo/misc/qr_demo.py
@@ -1,6 +1,11 @@
 import pyqrcode
 
+"""
+pyqrcode
+pypng
+"""
+
 string="http://httpbin.org"
 qr=pyqrcode.create(string)
-#qr.png(file)      			#导出png
-print(qr.terminal()) 	#终端打印
+qr.png(file)      			#导出png
+print(qr.terminal()) 	  #终端打印
