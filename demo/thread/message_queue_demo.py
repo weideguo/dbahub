5ef96f2bad2a630c3c656d176e5bed2979a67523
wdg <2515717245@qq.com>
2018-08-29 10:20:54 +0800

    Create message_queue_demo.py

---
+++ demo/thread/message_queue_demo.py
@@ -0,0 +1,36 @@
+import os,time,random
+
+def write(q):
+
+    for value in ['A','B','C']:
+        print('write put: %s...'%value)
+        q.put(value)
+        sleep_time=random.random()*3
+        time.sleep(sleep_time)
+        #print('write put: %s...'%value)
+        #q.put(value)
+
+
+def read(q):
+    while True:
+        value = q.get(True)
+        if value=="QUIT":
+            print("read now quit.....")
+            break
+        else:
+            print('read get: %s...'%value)
+
+
+if __name__ == '__main__':
+    print('start...')
+    q = Queue()
+    pw = Process(target=write,args=(q,))
+    pr = Process(target=read,args=(q,))
+
+    pw.start()
+    pr.start()
+
+    pw.join()
+    q.put('QUIT')
+    pr.join()
+    print('done...')
