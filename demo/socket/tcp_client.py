8075d93638938bd79e5c6349fe7b21a64d40dda8
wdg <2515717245@qq.com>
2018-08-29 23:36:51 +0800

    Create tcp_client.py

---
+++ demo/socket/tcp_client.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.connect(('127.0.0.1',9911))
+
+print s.recv(1024)
+
+for data in ['zhang','liu','wang']:
+	s.send(data)
+    	print s.recv(1024)
+
+s.send('exit')
+s.close()
