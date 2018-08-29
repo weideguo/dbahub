5d06cfd58f5ea2e36a8738bbbf551bc8e41bd0e5
wdg <2515717245@qq.com>
2018-08-29 09:40:29 +0800

    Update mongo_conn.py

--- demo/mongodb/mongo_conn.py
+++ demo/mongodb/mongo_conn.py
@@ -1,7 +1,7 @@
 #--encoding=utf-8
 '''
 import pymongo
-conn=pymongo.Connection("mongodb://192.168.200.141:27017")
+conn=pymongo.Connection("mongodb://192.168.1.1:27017")
 #没有？
 '''
 
@@ -10,13 +10,13 @@ from pymongo import MongoClient
 conn=MongoClient("localhost",27017)
 db=conn.test					#切换到db
 col=db.myc						#切换到collection
-dic={"name":"sysu","address":"guangzhou"}
+dic={"name":"sysu","address":"ggggggg"}
 col.insert(dic)                 #insert_one()  insert_many()参数为list
 
 for item in col.find():			#可以传入字典参数进行条件查询 有find_one()
 	print item
 	
-col.update({"name":"weideguo"},{"set":{"name":"sysuuuuu"}})
+col.update({"name":"wwwwwwww"},{"set":{"name":"sysuuuuu"}})
 
 col.remove({"name":"sysuuuuu"})
 
@@ -25,7 +25,7 @@ is_primary
 
 '''
 #使用SCRAM-SHA-1认证时
-client=pymongo.MongoClient('mongodb://dba:dba@127.0.0.1:27020/admin')
+client=pymongo.MongoClient('mongodb://m_user:m_password@127.0.0.1:27020/admin')
 client.admin.command('ismaster')
 '''
 '''
