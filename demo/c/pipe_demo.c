4c8994f3d91b0ae113fcb0716b4e63f2865449d7
wdg <2515717245@qq.com>
2018-08-29 14:51:58 +0800

    Update pipe_demo.c

--- demo/c/pipe_demo.c
+++ demo/c/pipe_demo.c
@@ -2,33 +2,32 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-/*
-* 进程使用管道进行通信
-*/
+
 
 int main()
 {
     int fd[2];
-    int ret = pipe(fd);  // fd为两个文件描述符,fd[0]指向管道的读端,fd[1]指向管道的写端。fd[1]的输出是fd[0]的输入。
+    int ret = pipe(fd);  // fd参数返回两个文件描述符,fd[0]指向管道的读端,fd[1]指向管道的写端。fd[1]的输出是fd[0]的输入。
     if (ret == -1)
     {
         perror("pipe error\n");
         return 1;
     }
     pid_t id = fork();
-    if (id == 0)  //child
+    if (id == 0)
     {
         close(fd[0]);
-        char *child = "I am  child!";
+        char *input = "I am  child!";
         int i = 0;
         while (i<5)
         {
-            write(fd[1], child, strlen(child) + 1);
+            printf("input: %s\n",input);
+            write(fd[1], input, strlen(input));
             sleep(2);
             i++;
         }
     }
-    else if (id>0)  //father
+    else if (id>0)
     {
         close(fd[1]);
         char msg[100];
@@ -39,9 +38,9 @@ int main()
             ssize_t s = read(fd[0], msg, sizeof(msg));
             if (s>0)
             {
-                msg[s - 1] = "\0";
+                msg[s] = "\0";
             }
-            printf("%s\n", msg);
+            printf("output: %s\n", msg);
             j++;
         }
     }
