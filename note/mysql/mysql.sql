d4441064f3363792959b7143ec0821f1f3d50474
wdg <2515717245@qq.com>
2018-10-31 11:26:44 +0800

    Update mysql.sql

--- note/mysql/mysql.sql
+++ note/mysql/mysql.sql
@@ -1094,7 +1094,7 @@ mysqlbinlog  log_name    ----查看二进制日志的内容
 	--start-position
 	--stop-position
 	--base64-output=decode-rows        ##导出row格式   只能用于查看，恢复的时候不要加这个参数
-	-v    				   ##导出成sql格式 
+	-v    				   ##导出成sql格式  与--base64-output一同使用
 	
 --从远端读取二进制日志并保存
 mysqlbinlog -u root -p -P 3306 -h host_name -R -r save_as_text_file_name remote_binlog_name
