d9530d4103deee3a1341125a785df24f6fce01e2
wdg <2515717245@qq.com>
2018-11-06 15:26:14 +0800

    Update qr_demo.py

--- demo/misc/qr_demo.py
+++ demo/misc/qr_demo.py
@@ -7,5 +7,6 @@ pypng
 
 string="http://httpbin.org"
 qr=pyqrcode.create(string)
-qr.png(file)      			#导出png
+file_path="./test.png"
+qr.png(file_path,scale=5)      			#导出png
 print(qr.terminal()) 	  #终端打印
