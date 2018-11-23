8a7dec2c3096d9f7274b34c0f21cf98ff60abca2
wdg <2515717245@qq.com>
2018-11-23 12:18:29 +0800

    Create path_demo.py

---
+++ demo/misc/path_demo.py
@@ -0,0 +1,41 @@
+
+export PYTHONPATH="/path_to_python_project_home"             
+#通过修改环境变量确定python项目的根目录，从而可以导入其他模块
+
+
+##############################################################################
+
+
+import os
+import sys
+
+
+print __file__                       
+print os.path.dirname(__file__)                        #为相对路径
+
+print sys.argv[0]
+print os.path.realpath(sys.argv[0])
+print os.path.dirname(os.path.realpath(sys.argv[0]))    #为绝对路径
+
+
+sys.path.append(os.path.dirname(os.path.realpath(sys.argv[0]))+"/../")      #切换到上层目录以导入其他模块
+
+
+
+
+##################################################################################
+python -m pack1.subpack1.real_name                     #以-m方式启动 pack1/subpack1/real_name.py  即可以pack1为路径导入其他模块 
+
+
+###real_name.py
+import pack1.subpack2
+
+
+####################
+pack1
+|
++-----------+
+|           |  
+subpack1   subpack1
+|
+real_name.py
