a3838b3527320c0127b9516321d61e35d7f36a92
wdg <2515717245@qq.com>
2018-12-07 10:55:35 +0800

    Update encode_decode.py

--- demo/misc/encode_decode.py
+++ demo/misc/encode_decode.py
@@ -20,14 +20,15 @@ reload(sys)
 sys.setdefaultencoding('utf8')   #文件的编码为utf8
 
 
-a="中文"            #由于在GBK编码下输入 所以a的编码为 '\xd6\xd0\xce\xc4'
-b=a.decode('gbk')   #由GBK转成unicode   u'\u4e2d\u6587'
+a="中文"            #在GBK编码下输入 a的编码为               '\xd6\xd0\xce\xc4'
+b=a.decode('gbk')   #由GBK转成unicode                      u'\u4e2d\u6587'
 
-a="中文"            #由于在utf8编码下输入 所以a的编码为 '\xe4\xb8\xad\xe6\x96\x87'
-b=a.decode('utf8')  #由utf8转成unicode  u'\u4e2d\u6587'
+a="中文"            #在utf8编码下输入 a的编码为              '\xe4\xb8\xad\xe6\x96\x87'
+b=a.decode('utf8')  #由utf8转成unicode                     u'\u4e2d\u6587'
 
-a=u"中文"           #无论出于那种编码环境 a的编码均为unicode  u'\u4e2d\u6587'
-
+a=u"中文"           #无论处于哪种编码环境 a的编码均为unicode  u'\u4e2d\u6587'
+b=a.encode('utf8')  #由unicode编码转成utf8编码              '\xe4\xb8\xad\xe6\x96\x87'
+b=a.encode('gbk')   #由unicode编码转成gbk编码               '\xd6\xd0\xce\xc4'
 
 #只要包含coding 及 utf8 即可
 #coding=utf8                     
