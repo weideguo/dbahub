13622391efbee9668d09f7c01414737fab097263
wdg <2515717245@qq.com>
2018-12-07 10:35:08 +0800

    Update qr_demo.py

--- demo/misc/qr_demo.py
+++ demo/misc/qr_demo.py
@@ -9,5 +9,5 @@ a="http://httpbin.org"
 a=u"中文"
 qr=pyqrcode.create(a)
 file_path="./test.png"
-qr.png(file_path,scale=5)      			#导出png
+qr.png(file_path,scale=5)           #导出png
 print(qr.terminal()) 	              #终端打印
