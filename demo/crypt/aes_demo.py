5d4e84d451810a725418e2e427b054f9e14ec618
wdg <2515717245@qq.com>
2018-12-19 10:31:34 +0800

    Update aes_demo.py

--- demo/crypt/aes_demo.py
+++ demo/crypt/aes_demo.py
@@ -10,14 +10,14 @@ key="qwertyuiopasdfgh"    #加密时使用的key，只能是长度16字节（16*
 
 def aes_encrypt(data,key):  
     BS = AES.block_size
-    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+    pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)    #0-255（十进制）对应的扩展ascii码
     cipher = AES.new(key)
     encrypted = cipher.encrypt(pad(data))  #aes加密
-    result = base64.b64encode(encrypted)  #base64 encode
+    result = base64.b64encode(encrypted)   #base64 encode
     return result
 
 def aes_decrypt(en_data,key):
-    unpad = lambda s : s[0:-ord(s[-1])]     #ord 8进制对应的ascii码
+    unpad = lambda s : s[0:-ord(s[-1])]     #ord 扩展ascii码的对应10进制数字（0-255）
     cipher = AES.new(key)
     result2 = base64.b64decode(en_data)
     decrypted = unpad(cipher.decrypt(result2))
