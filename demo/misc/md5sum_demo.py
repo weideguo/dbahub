cd55e3fb60dcc922026fd86cf823c8d77b31f101
wdg <2515717245@qq.com>
2018-12-25 11:26:53 +0800

    Update md5sum_demo.py

--- demo/misc/md5sum_demo.py
+++ demo/misc/md5sum_demo.py
@@ -4,7 +4,7 @@
 
 import hashlib
 
-def hash_bytestr_iter(bytesiter, hasher=hashlib.md5(), hex_str=True):
+def hash_bytestr_iter(bytesiter, hasher, hex_str=True):
     for block in bytesiter:
         hasher.update(block)
     return (hasher.hexdigest() if hex_str else hasher.digest())
@@ -17,11 +17,23 @@ def file_block_iter(filename, blocksize=65536):
             block = afile.read(blocksize)
 
 
+def my_md5(filename=None,str=None):
+    hasher=hashlib.md5()
+    if filename:
+        return hash_bytestr_iter(file_block_iter(filename),hasher)
+    elif str:
+        try:
+            str=str.encode("utf8")
+        except:
+            str=str
+        return hash_bytestr_iter([str],hasher)
+    else:
+        return None
       
-hash_bytestr_iter(file_block_iter("/root/ansible-2.7.4.zip"))  
+          
+hash_bytestr_iter(file_block_iter("/root/ansible-2.7.4.zip"),hashlib.md5())  
     
-    
-    
+       
 ######################################################################
 # simple demo    
         
