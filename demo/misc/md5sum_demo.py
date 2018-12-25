3ff01ba1c1319d6b7afd6652a30accf98d699019
wdg <2515717245@qq.com>
2018-12-25 11:56:57 +0800

    Update md5sum_demo.py

--- demo/misc/md5sum_demo.py
+++ demo/misc/md5sum_demo.py
@@ -1,43 +1,6 @@
-########################################################################
-# 大文件时防止出现内存不够用 分块读取
 
-
-import hashlib
-
-def hash_bytestr_iter(bytesiter, hasher, hex_str=True):
-    for block in bytesiter:
-        hasher.update(block)
-    return (hasher.hexdigest() if hex_str else hasher.digest())
-
-def file_block_iter(filename, blocksize=65536):
-    with open(filename, "rb") as afile:
-        block = afile.read(blocksize)
-        while len(block) > 0:
-            yield block
-            block = afile.read(blocksize)
-
-
-def my_md5(filename=None,str=None):
-    hasher=hashlib.md5()
-    if filename:
-        return hash_bytestr_iter(file_block_iter(filename),hasher)
-    elif str:
-        try:
-            str=str.encode("utf8")
-        except:
-            str=str
-        return hash_bytestr_iter([str],hasher)
-    else:
-        return None
-      
-          
-hash_bytestr_iter(file_block_iter("/root/ansible-2.7.4.zip"),hashlib.md5())  
-    
-       
-######################################################################
-# simple demo    
-        
-    
+# coding:utf8
+            
 import hashlib   
 
 src=u"askakirfkjhrzf这个"
