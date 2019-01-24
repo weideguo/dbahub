989942ab3b4f28547814c25365438aaeb1acf6d1
wdg <2515717245@qq.com>
2019-01-24 18:46:47 +0800

    Create daemon_demo.py

---
+++ demo/misc/daemon_demo.py
@@ -0,0 +1,25 @@
+#pip install python-daemon
+
+import os
+import time
+from daemon.runner import DaemonRunner
+ 
+_base_path="/tmp/"
+class MyApp(object):
+ 
+ 
+    stdin_path = "/dev/null"
+    stdout_path = os.path.join(_base_path, "myapp.stdout")
+    stderr_path =  os.path.join(_base_path, "myapp.stderr")
+    pidfile_path =  os.path.join(_base_path, "myapp.pid")
+    pidfile_timeout = 5
+ 
+    def run(self):
+        for i in range(10):
+            print i
+            time.sleep(5)
+ 
+if __name__ == '__main__':
+    run = DaemonRunner(MyApp())
+    run.do_action()
+    
