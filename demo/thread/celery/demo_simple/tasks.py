7ae6931e4674972f24ab2e44cafb43057ca43622
wdg <2515717245@qq.com>
2019-02-13 17:39:04 +0800

    Update tasks.py

--- demo/thread/celery/demo_simple/tasks.py
+++ demo/thread/celery/demo_simple/tasks.py
@@ -3,6 +3,10 @@
 import time
 from celery import Celery
 
+"""
+broker/backend 可以使用rabbitMQ
+"""
+
 app = Celery("tasks",broker="redis://:my_passwd@127.0.0.1:6379/5",backend="redis://:my_passwd@127.0.0.1:6379/6")
 
 @app.task
