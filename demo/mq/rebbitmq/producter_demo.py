dfcf4af4a60af8d4f0c8b661f906b564b69fa7a3
wdg <2515717245@qq.com>
2019-02-14 16:50:57 +0800

    Create producter_demo.py

---
+++ demo/mq/rebbitmq/producter_demo.py
@@ -0,0 +1,21 @@
+import pika
+
+
+username = "guest"                                                                                    
+pwd = "guest"
+cred = pika.PlainCredentials(username, pwd)
+conn = pika.BlockingConnection(pika.ConnectionParameters("192.168.59.132", credentials=cred))   
+channel = conn.channel()                                                                             
+
+
+#这样方式发送只能一个消费者接受   
+channel.queue_declare(queue="hello")         
+channel.basic_publish(exchange="", routing_key="hello",body="hello world")                              
+
+
+#实现广播                                                                                                
+channel.exchange_declare(exchange="logs",exchange_type="fanout")
+channel.basic_publish(exchange="logs", routing_key="",body="hello world")     
+  
+
+conn.close()    
