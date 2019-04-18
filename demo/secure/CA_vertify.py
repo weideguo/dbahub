e0247ef3398bf4e6ca0474a138eb236bb4fe5ad3
wdg <2515717245@qq.com>
2019-04-18 11:55:21 +0800

    Create CA_vertify.py

---
+++ demo/secure/CA_vertify.py
@@ -0,0 +1,29 @@
+##CA证书的验证
+
+
+
+
+
+##离线验证
+import OpenSSL
+f=open('./ca.crt','r')
+x=OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM,f.read())
+x.get_subject()
+x.get_issuer()
+
+
+
+
+#在线验证
+import ssl, socket
+
+hostname = 'www.google.com'
+ctx = ssl.create_default_context()
+s = ctx.wrap_socket(socket.socket(), server_hostname=hostname)
+s.connect((hostname, 443))
+cert = s.getpeercert()
+
+subject = dict(x[0] for x in cert['subject'])
+issued_to = subject['commonName']
+issuer = dict(x[0] for x in cert['issuer'])
+issued_by = issuer['commonName']
