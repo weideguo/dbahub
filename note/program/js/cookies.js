284f4bc5e846554c9f0aa999fdb63af06928e4a7
wdg <2515717245@qq.com>
2019-05-06 09:47:48 +0800

    Update cookies.js

--- note/program/js/cookies.js
+++ note/program/js/cookies.js
@@ -8,3 +8,6 @@ document.cookie="mykey_name=xxx;expires=expire_time;path=/"
 //获取
 cookie_lenght=document.cookie.length
 document.cookie.substring(0,cookie_lenght)
+
+
+//使用浏览机器时 访问url则该网站的cookie会自动全部提交
