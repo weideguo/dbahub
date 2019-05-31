64ae61b6c821a384f2cbb893c077d004b2bcf202
wdg <2515717245@qq.com>
2019-06-01 01:21:21 +0800

    Add files via upload

---
+++ demo/misc/mongtePI.py
@@ -0,0 +1,15 @@
+#coding:utf8
+#使用蒙特卡洛算法计算Π
+
+import random
+
+
+
+def montePI(n=100):
+	s=0
+	for i in range(n):
+		x=random.random()
+		y=random.random()
+		if (x*x+y*y)<1 :
+			s=s+1
+	return 4*s/n
