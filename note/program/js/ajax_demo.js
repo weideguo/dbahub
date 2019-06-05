304047c32cb1859bfeefd7fac2de48ffa53ada9c
wdg <2515717245@qq.com>
2019-06-05 15:31:13 +0800

    Create ajax_demo.js

---
+++ note/program/js/ajax_demo.js
@@ -0,0 +1,36 @@
+// AJAX  Asynchronous  JavaScript And XML  //js服务器交换数据
+
+var xmlhttp;
+
+xmlhttp=new XMLHttpRequest();                    //window.XMLHttpRequest
+// xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");  //IE old
+
+xmlhttp.open("GET","https://some_host/demo_get?p1=111&p2=222",true);    // 服务端需要设置headers Access-Control-Allow-Origin:*
+//open(method,url,async)  async：true（异步）或 false（同步）
+xmlhttp.send();
+
+//POST方法
+xmlhttp.open("POST","https://some_host/demo_post",true);
+xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");  //设置headers
+xmlhttp.send("fname=Bill&lname=Gates");
+
+
+/*
+onreadystatechange	存储函数（或函数名），每当 readyState 属性改变时，就会调用该函数。
+readyState	
+存有 XMLHttpRequest 的状态。从 0 到 4 发生变化。
+0: 请求未初始化
+1: 服务器连接已建立
+2: 请求已接收
+3: 请求处理中
+4: 请求已完成，且响应已就绪
+*/
+// 必须通过此实现对返回结果的调用
+xmlhttp.onreadystatechange=function(){
+    if (xmlhttp.readyState==4 && xmlhttp.status==200){  
+        document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+    }
+}
+
+//responseText	获得字符串形式的响应数据。
+//responseXML	  获得 XML 形式的响应数据
