767b3cae21edfa6c5a97483a1d129b353d1f588d
wdg <2515717245@qq.com>
2019-07-09 17:02:36 +0800

    Create ctypes_demo.py

---
+++ demo/misc/ctypes_demo.py
@@ -0,0 +1,32 @@
+#python调用动态连接库
+
+#windows#########################################################################################
+from ctypes import *
+
+#两总引入方式均可以              
+libc = windll.msvcrt
+libc.time()
+
+libc = cdll.msvcrt               
+libc.time()                      
+
+#windows动态来链接库
+#位于windows的动态连接库的路径下 C:\Windows\System32 
+#kernel32.dll 内存管理、数据的输入输出操作和中断处理
+#msvcrt.dll   printf,malloc,strcpy等C语言库函数的具体运行实现
+
+
+
+#linux#########################################################################################
+from ctypes import *
+        
+#两种引入方式        
+libc = cdll.LoadLibrary("libc.so.6")  
+libc.time(None)       
+     
+libc = CDLL('libc.so.6')         
+libc.time(None)                  
+
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/paht_2_so    #指定使用动态连接库的路径
+#调用动态连接库中的函数
+
