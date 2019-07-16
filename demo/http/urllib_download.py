4ab3ac34c21b9f3fb1d6e95f74c2cab3fd62e29f
wdg <2515717245@qq.com>
2019-07-16 16:32:50 +0800

    Delete urllib_download.py

--- demo/http/urllib_download.py
+++
@@ -1,8 +0,0 @@
-#!/bin/env python
-#--encoding=utf-8
-from urllib import urlopen
-
-f=open('/data/Demo2.zip','wb')            	  
-u=urlopen('https://github.com/rest-client/rest-client/archive/master.zip')
-f.write(u.read())
-f.close()
