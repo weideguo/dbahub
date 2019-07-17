08dfde5e9085d00dc84e80b223472f64b3223f97
wdg <2515717245@qq.com>
2019-07-17 12:12:18 +0800

    Update decorator_demo.py

--- demo/misc/decorator_demo.py
+++ demo/misc/decorator_demo.py
@@ -1,13 +1,19 @@
 #装饰器
 #coding=utf-8
 
-
 def decorator1(func):
     def my_wrapper():		
         print "bDFGER"
         func()
-    return my_wrapper	
+    return my_wrapper
 
+	
+#不可接受任何参数	
+@decorator1
+def f1():
+    print 'rr'
+	
+######################################################   
 
 def decorator2():
     def _decorator2(func):
@@ -17,6 +23,15 @@ def decorator2():
         return my_wrapper
     return _decorator2
 
+    
+#不可接受任何参数
+@decorator2()
+def f2():
+    print 'yyy'   
+    
+     
+######################################################     
+   
 def decorator3(func): 
     def my_wrapper(*a,**b):
         print a
@@ -24,34 +39,49 @@ def decorator3(func):
         func(*a,**b)
     return my_wrapper
 
-
-def thread_control(x):
-    def _thread_control(func):
-        def my_wrapper(*a,**b):
-            print a
-            print b
-            #print x	
-            func(*a,**b)
-        return my_wrapper
-    return _thread_control
-
-@thread_control(x='wert')		
-def set_value_by_host(t):
-	print t
-
-@decorator2()
-def f():
-    print "a"
-    print "b"
-
+    
+#函数可以接受参数 装饰器不可以接受参数
 @decorator3
 def f3(*a,**b):
     print a
     print b
 
+f3('ref','re',rf='rtb',g='r')
 
-if  __name__ == '__main__':
-    print "%s"%time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))	
-    #set_value_by_host("hello")
-    f3('ref','re',rf='rtb',g='r')
-    print "%s"%time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+################################################################################    
+    
+def decorator4(x=None):
+    print x,1                 #使用@decorator4时即调用
+    def _decorator4(func):
+        print x,2             #使用@decorator4时即调用
+        return func
+    return _decorator4  
+   
+
+#装饰器 函数的参数都可以有 装饰器内获取只能获取装饰器函数的参数
+@decorator5('yyy')
+def x(n,m):
+    print n,m
+   
+
+##############################################################################
+
+def decorator5(x=None):
+    print x,1                   #使用@decorator5时即调用
+    def _decorator5(func):
+        print x,2               #使用@decorator5时即调用
+        def my_wrapper(*a,**b):
+            print x,3           #调用被装饰的函数时才调用
+            print a
+            print b	
+            func(*a,**b)
+        return my_wrapper
+    return _decorator5
+
+
+#装饰器 函数的参数都可以有 而且可以在装饰器内获取
+@decorator5(x='wert')		
+def x(t):
+	print t
+    
