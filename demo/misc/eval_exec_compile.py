3d9355ea68c93bcc2241263003b9053c8a3be397
wdg <2515717245@qq.com>
2019-07-25 18:14:15 +0800

    Create eval_exec_compile.py

---
+++ demo/misc/eval_exec_compile.py
@@ -0,0 +1,28 @@
+#在代码中执行python代码
+
+s="""
+for i in range(10):
+    print(i)
+"""
+
+exec(s)         #执行python代码 没有返回值
+
+a="{'a':'aaa'}"
+def f():
+    print("aaa")
+
+b="f"
+a0=eval(a)     #执行单个表达式python代码 有返回值
+b0=eval(b)
+
+
+
+c = compile(s,'','exec')
+exec(c)
+
+c = compile(b,'','eval')
+f1=eval(c)
+
+single_str = 'a = input("Input a number: ")'
+code_single = compile(single_str, '', 'single')   #交互式
+eval(code_single)
