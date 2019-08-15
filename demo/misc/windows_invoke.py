5e82907f55dcaed09f95f17997a95e37b392d2f9
wdg <2515717245@qq.com>
2019-08-15 15:33:28 +0800

    Update windows_invoke.py

--- demo/misc/windows_invoke.py
+++ demo/misc/windows_invoke.py
@@ -1,5 +1,6 @@
 #通过dll调用windows的函数
 
 from ctypes import *  
+#默认在C:\Windows\System32目录下 可以指定完整目录
 user32 = windll.LoadLibrary('user32.dll')  
 user32.MessageBoxA(0,'this is content','this is title',0) 
