5f46dce39643d4d3ad2a6d723f9dfda384ecaef7
zouzhicun <2515717245@qq.com>
2019-09-12 16:39:03 +0900

    update

--- demo/misc/class_demo.py
+++ demo/misc/class_demo.py
@@ -1,4 +1,4 @@
-class A():
+class A(B):
     #构造函数 不应该有return
     def __init__(self,...):
     
@@ -8,7 +8,34 @@ class A():
     def __a(self):
         print "__a"
     
+    #可以外部调用 继承
     def _a(self):
         self.__a()
         print "_a"
-     
+
+        #调用父方法
+        super(A,self).__init__()
+    
+
+       
+        
+        
+#多重继承
+class D(E,F)
+    pass
+        
+        
+#Method Resolution Order(方法解析顺序) 
+#查看D的mro        
+D.mro()     
+
+#super(cls, inst) 获得的是 cls 在 inst 的 MRO 列表中的下一个类
+#super函数的工作原理
+def super(cls,inst):
+    mro=inst.__class__.mro()
+    return mro[mro.index(cls)+1]
+    
+    
+    
+__class__ 获取实例对应的类
+    
