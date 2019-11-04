4b7f690e1de410a0ef41a0967295eeb7eee64d97
zouzhicun <2515717245@qq.com>
2019-11-04 12:32:37 +0900

    update

---
+++ demo/misc/dir_demo.py
@@ -0,0 +1,14 @@
+
+import os
+
+
+#获取当前工作目录
+os.getcwd()
+
+
+#修改当前工作目录
+path="/tmp"
+os.chdir( path )
+
+
+
---
+++ demo/misc/process_name.py
@@ -0,0 +1,9 @@
+#coding:utf8
+#设置进程名
+#ps -ef | grep "xxyy"
+
+import setproctitle
+setproctitle.setproctitle("xxyy")
+
+
+
