475ec81ce3000aefd4b2622264ce552abf7b66e6
zouzhicun <2515717245@qq.com>
2020-05-05 23:37:49 +0800

    update

--- demo/secure/crypt/rsa_demo.py
+++ demo/secure/crypt/rsa_demo.py
@@ -1,6 +1,6 @@
 from Crypto.PublicKey import RSA
 
-
+################################################pycypto 不建议再使用；使用pycryptodome代替
 """
 openssl genrsa -out mykey.pem                  #私钥
 
@@ -16,3 +16,13 @@ en_str=pub_key.encrypt(text)
 
 pri_key=RSA.importKey(open('mykey.pem'))     #使用私钥解密
 de_str=pri_key.decrypt(en_str[0])
+
+
+############pycryptodome
+"""
+The Public-Key Cryptography Standards (PKCS)
+公钥密码学标准，
+包括证书申请、证书更新、证书作废表发布、扩展证书内容以及数字签名、数字信封的格式
+等方面的一系列相关协议。
+PKCS#1 ... PKCS#15
+"""
