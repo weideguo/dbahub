751d2f587545fb25a727ee52f1e2bbb2f74c5511
weideguo <2515717245@qq.com>
2021-05-21 19:13:15 +0800

    update

---
+++ others/go/aSimpleTest/random_demo.go
@@ -0,0 +1,61 @@
+package main
+
+
+import (
+    "fmt"
+    // "math"
+    "math/rand"
+    "time"
+)
+
+
+func main() {
+    rand.Seed(time.Now().UTC().UnixNano())
+    fmt.Printf("%9f \n", rand.Float64())
+    fmt.Printf("%d \n", 1_000)
+    fmt.Printf("%d \n", rand.Intn(3))
+}
+
+
+/*
+import (
+    "fmt"
+    "math"
+    "math/rand"
+    "time"
+)
+
+func main() {
+    rand.Seed(time.Now().UTC().UnixNano())
+
+    const (
+        e           = math.E
+        experiments = 10
+    )
+
+    fmt.Printf("Estimating e with %d experiment(s).\n\n", experiments)
+
+    var acc int
+    for i := 0; i < experiments; i++ {
+        var (
+            sum         float64
+            num2Success int
+        )
+
+        for sum <= 1 {
+            n := rand.Float64()
+            sum += n
+            num2Success++
+        }
+        acc += num2Success
+        fmt.Printf("%d %9f\n",acc, sum)
+    }
+
+    expected := float64(acc) / float64(experiments)
+    errorPct := 100.0 * math.Abs(expected-e) / e
+
+    fmt.Printf("Expected vale: %9f \n", expected)
+    fmt.Printf("e: %9f \n", e)
+    fmt.Printf("Error: %9f%%\n", errorPct)
+}
+*/
