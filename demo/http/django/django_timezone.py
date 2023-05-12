a7aaafae16784f1b4a7ec35eaeb0944f87eb8fed
weideguo <2515717245@qq.com>
2023-05-12 18:59:43 +0800

    update

---
+++ demo/http/django/django_timezone.py
@@ -0,0 +1,5 @@
+
+from django.utils import timezone
+timezone.now()           # USE_TZ = False 时，为TIME_ZONE指定时区的时间；USE_TZ = True 时，则为utc+0时间
+
+
--- others/go/aSimpleTest/type_demo3.go
+++ others/go/aSimpleTest/type_demo3.go
@@ -13,6 +13,11 @@ type Scroll2 struct {
     C string
 }
 
+type Scroll3 struct {
+    Scroll2
+    D string
+}
+
 func (s *Scroll) SetA(a int){
     s.A = a
 }
@@ -22,8 +27,16 @@ func (s *Scroll) SetB(b string){
 }
 
 
+func (s *Scroll2) SetB(b string){
+    s.B = b+"22222"
+}
+
 func (s *Scroll2) SetC(c string){
     s.C = c
+}
+
+func (s *Scroll3) SetD(d string){
+    s.D = d
 }
 
 func main(){
@@ -35,6 +48,14 @@ func main(){
     s2.SetA(1)
     s2.SetB("bbb2")
     s2.SetC("ccc")
+    
+    s3 := &Scroll3{}
+    s3.SetA(1)
+    s3.SetB("bbb3")
+    s3.SetC("ccc3")
+    s3.SetD("ddd3")
     fmt.Println(s1)
     fmt.Println(s2)
+    fmt.Println(s3)
+    fmt.Println(s3.A)
 }
