2aa3735d5a43c755094246fda4b65fc13b5c9148
weideguo <2515717245@qq.com>
2023-05-26 21:35:23 +0800

    update

---
+++ demo/misc/gradio_demo.py
@@ -0,0 +1,17 @@
+# 一个演示页面
+
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+demo.launch(server_name="0.0.0.0")  
+
+"""
+fn        处理输出的函数
+inputs    控制输入的样式 可以为字符串、列表  列表的个数应该与函数的输入参数个数相同
+outputs   控制输出的样式 可以为字符串、列表  列表的个数应该与函数的返回参数个数相同
+"""
+
